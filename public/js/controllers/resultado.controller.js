b20e8a347957c48dd981e6feffc0a78dac75e21a
Nicolas Fernandez <fernandez.nicolas.j@gmail.com>
2015-10-17 00:11:57 -0300

    Remove public folder

--- public/js/controllers/resultado.controller.js
+++
@@ -1,9 +0,0 @@
-app.controller('ResultCtrl', ['$scope','$stateParams','QueryFac', function($scope,$stateParams,QueryFac){
-	
-	QueryFac.searchQuery($stateParams.query).then(function(response){
-		$scope.resultados = response.data.results;
-	}, function(error){
-		console.log(error);
-	});
-
-}]);
--- public/js/controllers/search.controller.js
+++
@@ -1,8 +0,0 @@
-app.controller('SearchCtrl', ['$scope','$state', function($scope,$state){
-	
-	// Funcion para realizar la busqueda sobre la API de ML y llamar a la vista de resultado
-	$scope.buscarQuery = function(){
-		$state.go('resultado',{ query : $scope.query });
-	}
-
-}]);
