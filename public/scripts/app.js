0420c926d1a9944e3aead6aa25d6a26309cd2824
Nicolas Fernandez <fernandez.nicolas.j@gmail.com>
2015-10-17 15:34:22 -0300

    Home

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,16 @@
-import angular from 'angular';
-import core from './modules/core';
+// import angular from 'angular';
+// import core from './modules/core';
 
-angular.module('airbnb',[core.name]);
+// angular.module('airbnb',[core.name]);
 
-angular.element(document)
-	.ready(() => angular.bootstrap(document, ['airbnb']));
+// angular.element(document)
+// 	.ready(() => angular.bootstrap(document, ['airbnb']));
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
